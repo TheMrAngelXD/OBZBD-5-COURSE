1786cb5d3fc142932098f502ee50afe7c5da4c3c
TheMrAngelXD <kostenko.stas@mail.ru>
2022-12-13 20:12:19 +0300

    Fixed Minor Issues

--- PhotoStudioTests/OrdersTest.cs
+++ PhotoStudioTests/OrdersTest.cs
@@ -11,37 +11,33 @@ public class OrdersTest
     public void OrdersCreateTrue()
     {
         //Arrange
-        int idOrd = 2;
         string numOrder = "15";
-        int idCli = 17;
         int numClient = 12;
         string fullName = "Chizhov Ivan Vasilievich";
         string address = "RUT MIIT";
         string email = "chizhik@mail.ru";
-        string temp = $"{idOrd},{numOrder},{idCli},{numClient},{fullName},{address},{email}";
+        string temp = $"{numOrder},{numClient},{fullName},{address},{email}";
 
         //Act
-        var client1 = new Client(idCli, numClient, fullName, address, email);
-        var order2 = new Order(idOrd, numOrder, client1);
+        var client1 = new Client(numClient, fullName, address, email);
+        var order2 = new Order(numOrder, client1);
         //Asssert
-        //Assert.Equal(order2.ToString(), temp);
         Assert.Equal(order2.ToString(),temp);
+        Assert.Equal(order2.ToString(), temp);
     }
 
     [Fact]
     public void ExceptionCreateFalse()
     {
         //Arrange
-        int id = 2;
-        int idCli = 17;
         int numClient = 12;
         string fullName = "Chizhov Ivan Vasilievich";
         string address = "RUT MIIT";
         string email = "chizhik@mail.ru";
         //Act
-        var client1 = new Client(idCli, numClient, fullName, address, email);
+        var client1 = new Client(numClient, fullName, address, email);
         //Assert
-        Assert.Throws<ArgumentNullException>(() => new Order(id, null!, client1));
+        Assert.Throws<ArgumentNullException>(() => new Order(null!, client1));
 
     }
 
@@ -49,22 +45,19 @@ public class OrdersTest
     public void AddCreateTrue()
     {
         //Arrange
-        int id = 2;
         string numOrder = "213";
-        int idCli = 17;
         int numClient = 12;
         string fullName = "Chizhov Ivan Vasilievich";
         string address = "RUT MIIT";
         string email = "chizhik@mail.ru";
-        int idServ = 22;
         string numService = "77";
         string nameService = "Photo";
         string quantity = "1";
         string price = "30$";
         //Act
-        var client1 = new Client(idCli, numClient, fullName, address, email);
-        var service1 = new Service(idServ, numService, nameService, quantity, price);
-        var order1 = new Order(id, numOrder, client1);
+        var client1 = new Client(numClient, fullName, address, email);
+        var service1 = new Service(numService, nameService, quantity, price);
+        var order1 = new Order(numOrder, client1);
         order1.AddService(service1);
 
         //Assert
